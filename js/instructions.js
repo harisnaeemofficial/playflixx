5ad054518c41b85064837771449c8eeac90e4971
Elio Osés <eliooses@netfilmx.com>
2015-12-26 07:51:29 -0300

    Create instructions.js

---
+++ js/instructions.js
@@ -0,0 +1,46 @@
+/*
+	# playflix.github.io
+
+	Watch movies instantly powered by Hydraflix
+
+	It works in 4 simple steps:
+
+	**Step 1:** Install [this](https://chrome.google.com/webstore/detail/torrent-stream/icocmgpofpimcojhefbcfbdldkmndpgj/) https://chrome.google.com/webstore/detail/torrent-stream/icocmgpofpimcojhefbcfbdldkmndpgj/
+
+	**Step 2:** Install [this too!](https://chrome.google.com/webstore/detail/torrentstream-helper/ebaaacomjclfgnkmaeaneljmfgkofphc) https://chrome.google.com/webstore/detail/torrentstream-helper/ebaaacomjclfgnkmaeaneljmfgkofphc
+
+	**Step 3:** Visit [http://playflix.github.io](http://playflix.github.io)
+
+	**Step 4:** Right click on your favorite movie and select the "Open with TorrentStream" option.
+
+	* **Step 4.1:** ENJOY!
+
+	* **Step 4.2:** Appreciate and donate: [1ARqS2EgYprBnNTcaSXf6WdcMMtuSR96E2](bitcoin:1ARqS2EgYprBnNTcaSXf6WdcMMtuSR96E2)
+
+	Is secure (the extensions are storaged in Chrome Store), open (hosted and rendered in GitHub), free and without bullshit/garbage/ads.
+	
+	https://developer.chrome.com/webstore/branding
+	https://developer.chrome.com/webstore/inline_installation?csw=1
+
+	Detect if the extension is installed:
+		https://developer.chrome.com/extensions/extension#global-events
+		http://stackoverflow.com/questions/6293498/check-whether-user-has-a-chrome-extension-installed
+
+	CLIENTS:
+	https://popcorntime.re
+	https://popcorntime.ml
+	https://popcorntime.cc
+	https://popcorntimece.tk
+	http://popcorn-time.ru
+	http://popcorn-time.se
+	https://www.popcorntime.ws
+
+	APIS:
+	OUT OF SERVICE (?)
+	http://yify-torrents.com/api/
+	https://eztvapi.re/
+	https://popcorni33hocj37.onion.to/
+	https://odgoglfi7uddahby.onion.to/
+	https://mi2i2dospijuauxa.onion.to/
+	https://ptapinjktqtsweq7.onion.to/
+*/
