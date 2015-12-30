b3eafbd8f0df4604ae22b87b6b3c03b45de99045
Elio Osés <eliooses@netfilmx.com>
2015-12-30 15:46:53 -0300

    Update catalogue.js

--- js/catalogue.js
+++ js/catalogue.js
@@ -1,7 +1,8 @@
 $(function () {
-	
+
+	var trackers = "&tr=udp:\/\/open.demonii.com:1337\/announce&tr=http:\/\/tracker.trackerfix.com\/announce&tr=udp:\/\/9.rarbg.to:2710&tr=udp:\/\/9.rarbg.me:2710&tr=udp:\/\/exodus.desync.com:6969&tr=udp:\/\/tracker.coppersurfer.tk:6969&tr=udp:\/\/tracker.leechers-paradise.org:6969&tr=udp:\/\/tracker.openbittorrent.com:80&tr=udp:\/\/glotorrents.pw:6969\/announce&tr=udp:\/\/tracker.opentrackr.org:1337\/announce&tr=udp:\/\/torrent.gresille.org:80\/announce&tr=udp:\/\/p4p.arenabg.com:1337&tr=udp:\/\/tracker.internetwarriors.net:1337";
+
 	// Pagination | Infinite Scrolling
-	var limit_items = 24; // 12, 24, 48 (bootstrap friendly query must be divisible for 1, 2, 3 and 4)
 	var page_value = 0;
 	var pageToSave = JSON.stringify(page_value);
 	localStorage.setItem("pagina", pageToSave);
@@ -11,15 +12,28 @@ $(function () {
 
 	var proxy = "http://crossorigin.me/";
 
-	var protocol = "http://"
+	var protocol = "https://";
 
-	var endpoint = "api.torrentsapi.com/list?";
-	
-	var sort_key = "sort";
+	var endpoint = "ytsag";
+	switch (endpoint) {
+		case "torrentsapi": endpoint = "api.torrentsapi.com/list?"; break;
+		case "ytsag": endpoint = "yts.ag/api/v2/list_movies.json?"; break;
+		case "ytsis": endpoint = "yify.is/index.php/api/v2/list_movies.json?"; break;
+		case "ytsli": endpoint = "yts.li/api/v2/list_movies.json?"; break;
+
+		// default: endpoint = "";
+	}
+
+	var sort_key = "&sort_by=";
 	var sort_value = "seeds";
 	
-	var quality_key ="quality";
+	var quality_key ="&quality=";
 	var quality_value ="720p";
+
+	// 12, 24, 48 (bootstrap friendly query must be divisible for 1, 2, 3 and 4)
+	var limit_items_key = "&limit=";
+	var limit_items_value = 24;
+	var limit = limit_items_key+limit_items_value;
 
 	var page_key = "&page=";
 
@@ -41,57 +55,77 @@ $(function () {
 
 		var page = page_key+page_value;
 
-		var parameters = sort+quality+page;
+		var parameters = sort+quality+limit+page;
 
 		var api_url = proxy+protocol+endpoint+parameters;	
 		
-		console.log(api_url);	
+		// DATASET
+		var hash;
+		var imdb;
+		var magnet;
+		var title;
+		var rating;
+		var poster;
+		var genre;
+		var background;	
+		// DATASET
+
+		console.log(api_url);
 
 		$.getJSON(api_url, function (data) {
+			console.log(api_url);
+			// GET STATUS DATA
+			/*
+			#ToDo
+			Returned Key	Description	Example
+			status	The returned status for the API call, can be either 'ok' or 'error'	ok
+			status_message	Either the error message or the successful message	Query was successful
+			data	If 'status' is returned as 'ok' the API query results will be inside 'data'	data
+			*/
+			// GET STATUS DATA
 
 			i = 0;
-			var html = "";
 	
 			// JSON DATA
 	
-			var json_data = data.MovieList;			
+			var json_data_provider = "ytsag";
+			
+			switch (json_data_provider) {
+				case "torrentsapi": json_data=data.MovieList; break;
+				case "ytsag": json_data=data.data.movies; break;
+				case "ytsis": json_data=data.data.movies; break;
+				case "ytsli": json_data=data.data.movies; break;
+				// default: json_data = "";
+			}			
 	
 			// JSON DATA
-
 			$.each(json_data, function (i, item) {
-				if (item.items[0] === undefined) {
+				//console.log(item.items[0]);
+				var provider_name = "ytsag";
+				switch (provider_name) {
+					case "torrentsapi": hash=item.items[0].id; imdb=item.imdb; magnet=item.items[0].torrent_magnet; title=item.title; rating=item.rating; poster=item.poster_med; genre=item.genres[0]; background=item.poster_big; break;
+					case "ytsag": hash=item.torrents[0].hash; imdb=item.imdb_code; magnet="magnet:?xt=urn:btih:"+hash+"&dn="+escape(item.title)+trackers; title=item.title; rating=item.rating; poster=item.medium_cover_image; genre=item.genres[0]; background=item.background_image; break;
+					case "ytsis": hash=item.torrents[0].hash; imdb=item.imdb_code; magnet="magnet:?xt=urn:btih:"+hash+"&dn="+escape(item.title)+trackers; title=item.title; rating=item.rating; poster=item.medium_cover_image; genre=item.genres[0]; background=item.medium_cover_image; break;
+					case "ytsli": hash=item.torrents[0].hash; imdb=item.imdb_code; magnet="magnet:?xt=urn:btih:"+hash+"&dn="+escape(item.title)+trackers; title=item.title; rating=item.rating; poster=item.medium_cover_image; genre=item.genres[0]; background=item.background_image; break;
+					// default: provider = "";
+				}				
+
+				if (provider_name === "torrentsapi") {
+					if (item.items[0] === undefined) {
+					}
+					else {
+						catalogue (hash, imdb, magnet, title, rating, poster, genre, background);
+					}	
 				}
 				else {
-					var hash = item.items[0].id;
-					var imdb = item.imdb;
-					var magnet = item.items[0].torrent_magnet;
-					var title = item.title;
-					var rating = item.rating;
-					var poster = item.poster_med;
-					var genre = item.genres[0];
-					var background = item.poster_big;
-
-					html+='<div id="movie-box-'+'" class="movie-box movie-box-'+genre+' col-lg-2 col-md-3 col-sm-6 col-xs-12" style="position:relative;float:left;">';
-					html+='<div style="width: 100%; height: 330px; position: absolute;left: 0px; top: 0px; -webkit-filter: blur(3px); -moz-filter: blur(3px); -o-filter: blur(3px); -ms-filter: blur(3px); filter: blur(3px); background-image:url(http://crossorigin.me/'+background+'); background-position: center; background-size: cover;  background-repeat: no-repeat; -webkit-box-shadow: inset 0px 0px 30px 30px rgba(0, 0, 0, 1); -moz-box-shadow: inset 0px 0px 30px 30px rgba(0, 0, 0, 1); box-shadow: inset 0px 0px 30px 30px rgba(0, 0, 0, 1);"></div>';
-					html+='<a href="'+magnet+'">';
-					html+='<img id="img-movie-box-'+hash+'" class="hover-luz" title="'+title+'" alt="'+title+'" src="'+poster+'" style="width: 100%; max-width: 180px; height: 270px; position: relative;" onmouseover="hoverHash(&#39;'+imdb+'&#39;)" onmouseout="outHash(&#39;'+imdb+'&#39;)" onclick="f_play(&#39;'+hash+'&#39;,&#39;'+imdb+'&#39;,&#39;'+poster+'&#39;,&#39;'+background+'&#39;,&#39;'+rating+'&#39;); scrollDetails(&#39;'+hash+'&#39;);"/>';
-					html+='<div id="movie-rating-star-'+imdb+'" class="movie_rating_star"></div></div>';
-					html+='</a>';
-
-					$.get("http://www.omdbapi.com/?i="+imdb+"&plot=full&r=json", function (data) {
-						movie_rating = Math.round(data.imdbRating);
-						movie_rating_star = '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
-						movie_rating_star_empty = '<span class="glyphicon glyphicon-star" aria-hidden="true" style="color:#000;"></span>';
-						html_rating = movie_rating_star.repeat(movie_rating)+movie_rating_star_empty.repeat(10 - movie_rating);
-						$("#movie-rating-star-"+data.imdbID).html(html_rating);
-					});
-				}													
+					catalogue (hash, imdb, magnet, title, rating, poster, genre, background)
+				};												
 				i++;				
 			});	
 			// Pagination | Infinite Scrolling
 			page=page++;
 			// Pagination | Infinite Scrolling
-			$('#movies').append(html);
+			//$('#movies').append(html);
 		});	
 	}; //Eof loadMore()		
 	// Pagination | Infinite Scrolling		
