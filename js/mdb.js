77b19867bb6a0854dd98cfada4dff41f95551cca
elio <elio@netfilmx.com>
2017-12-11 01:37:44 -0300

    adding mdb.js

---
+++ js/mdb.js
@@ -0,0 +1,27 @@
+// http://www.imdb.com/interfaces/
+
+// http://www.omdbapi.com
+/*
+    $.getJSON("http://omdbapi.com/?t=" + movie.Title(), {
+            api_key: '7a5035cca9022ea67734112eb921d5d33680a63d'
+            */
+
+// var promise = $http.get("http://api.themoviedb.org/3/search/person?api_key=f24727bdb915ca05f7718876104b211d&query=" + $scope.searchName);
+
+// 		$http.jsonp("http://www.myapifilms.com/imdb/idIMDB?title=" + title + "&token=38156e0f-67c9-4027-af2c-8ac1d1d99398&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=2&exactFilter=0&limit=10&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=0&biography=0&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=0&actorTrivia=0&similarMovies=0&adultSearch=0&goofs=0&quotes=0&fullSize=0&callback=JSON_CALLBACK")
+
+// <img src="https://image.tmdb.org/t/p/w150{{movie.poster_path}}">
+
+// https://theimdbapi.org/api/find/movie?title=transformers&year=2007
+
+// https://www.themoviedb.org/documentation/api
+
+// https://imdb.p.mashape.com/movie | https://market.mashape.com/apathetic/imdb
+
+// https://devru-bigflix-movies-download-v1.p.mashape.com/
+
+// https://www.watchonline.to/page/api
+
+// http://imdbapi.poromenos.org/ #CHECKTHISOUT
+
+// https://rapidapi.com/user/professorhaseeb/package/Movie%20Database/ https://omovies.p.mashape.com/movie/tt0088247?mashape-key=6VqHPbVfAAmsh0iiJs1z7MDFuI8xp15WVXIjsnR7Ioh5gA3gAt
